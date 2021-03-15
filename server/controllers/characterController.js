b98c09193674addac9fcddc00b59ee8e2e0c9a3d
Chris Burton <chris@fullstackcaveman.com>
2021-03-14 23:05:20 -0600

    Set up route controller for characters

---
+++ server/controllers/characterController.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler';
+import Character from '../models/characterModel.js';
+
+// @desc   Fetch all characters
+// @route  GET /api/characters
+// @access Public
+const getCharacters = asyncHandler(async (req, res) => {
+	const characters = await Character.find({});
+
+	res.json(characters);
+});
+
+// @desc   Fetch single character
+// @route  GET /api/characters/:id
+// @access Public
+const getCharacterById = asyncHandler(async (req, res) => {
+	const character = await Character.findById(req.params.id);
+
+	if (character) {
+		res.json(character);
+	} else {
+		res.status(404);
+		throw new Error('Character not found');
+	}
+});
+
+export { getCharacters, getCharacterById };
--- server/routes/characterRoutes.js
+++ server/routes/characterRoutes.js
@@ -1,35 +1,11 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
-import Character from '../models/characterModel.js';
+import {
+	getCharacterById,
+	getCharacters,
+} from '../controllers/characterController.js';
 
-// @desc Fetch all characters
-// @route GET /api/characters
-// @access Public
-router.get(
-	'/',
-	asyncHandler(async (req, res) => {
-		const characters = await Character.find({});
-
-		res.json(characters);
-	})
-);
-
-// @desc Fetch single character
-// @route GET /api/characters/:id
-// @access Public
-router.get(
-	'/:id',
-	asyncHandler(async (req, res) => {
-		const character = await Character.findById(req.params.id);
-
-		if (character) {
-			res.json(character);
-		} else {
-			res.status(404);
-			throw new Error('Character not found');
-		}
-	})
-);
+router.route('/').get(getCharacters);
+router.route('/:id').get(getCharacterById);
 
 export default router;
