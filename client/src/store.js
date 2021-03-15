55f609903e03d13acae7d9bfa751202cd54bb30c
Chris Burton <chris@fullstackcaveman.com>
2021-03-14 22:50:46 -0600

    Set up CharacterInfo to pull state from redux

--- client/src/store.js
+++ client/src/store.js
@@ -2,10 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { chracterListReducer } from './reducers/characterReducers';
+import {
+	chracterListReducer,
+	characterInfoReducer,
+} from './reducers/characterReducers';
 
 const reducer = combineReducers({
 	characterList: chracterListReducer,
+	characterInfo: characterInfoReducer,
 });
 
 const initialState = {};
