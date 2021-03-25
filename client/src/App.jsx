3c201ca9a9ec6b156c66fcc77b92a54fb1129ed0
Chris Burton <chris@fullstackcaveman.com>
2021-03-24 18:05:57 -0600

    Fixed Pagination Dimming

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,12 +10,12 @@ import Home from './components/Home';
 import Planets from './components/planets/Planets';
 import Species from './components/species/Species';
 import Starships from './components/starships/Starships';
+import UserEdit from './components/users/UserEdit';
 import UserLoginForm from './components/forms/UserLoginForm';
 import UserList from './components/users/UserList';
 import UserProfile from './components/forms/UserProfile';
 import UserRegisterForm from './components/forms/UserRegisterForm';
 import Vehicles from './components/vehicles/Vehicles';
-import UserEdit from './components/users/UserEdit';
 
 const App = () => {
 	return (
@@ -26,15 +26,15 @@ const App = () => {
 				<Route path='/admin/characterList' component={CharacterList} />
 				<Route path='/admin/user/:id/edit' component={UserEdit} />
 				<Route path='/admin/userlist' component={UserList} />
-				<Route path='/users/login' component={UserLoginForm} />
-				<Route path='/users/profile' component={UserProfile} />
-				<Route path='/users/register' component={UserRegisterForm} />
 				<Route exact path='/characters' component={CharacterPage} />
 				<Route path='/characters/:id' component={CharacterInfo} />
 				<Route exact path='/films' component={Films} />
 				<Route path='/planets' component={Planets} />
 				<Route exact path='/species' component={Species} />
 				<Route path='/starships' component={Starships} />
+				<Route path='/users/login' component={UserLoginForm} />
+				<Route path='/users/profile' component={UserProfile} />
+				<Route path='/users/register' component={UserRegisterForm} />
 				<Route exact path='/vehicles' component={Vehicles} />
 				<Route exact path='/' component={Home} />
 			</Switch>
