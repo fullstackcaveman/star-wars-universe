9f4daa7867e14e1e873c39edceef50b4cea076e6
Chris Burton <chris@fullstackcaveman.com>
2021-04-08 22:13:27 -0600

    server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(function (req, res, next) {
 	res.setHeader(
 		'Content-Security-Policy',
-		"default-src 'self'; font-src 'self'; img-src 'self' data: https://starwars-visualguide.com/* https://starwars-visualguide.com/*; script-src 'self'; style-src 'self'; frame-src 'self'"
+		"default-src 'self'; font-src 'self'; img-src *; script-src 'self'; style-src 'self'; frame-src 'self'"
 	);
 	next();
 });
