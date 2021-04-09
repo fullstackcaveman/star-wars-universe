bf3e1856d4d8e8b56a2061fa01770aa23f4e3091
Chris Burton <chris@fullstackcaveman.com>
2021-04-08 20:53:16 -0600

    server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(function (req, res, next) {
 	res.setHeader(
 		'Content-Security-Policy-Report-Only',
-		"default-src 'self'; font-src 'self'; img-src 'self' https://cloud.mongodb.com/v2/604d43fd100168432629c328; script-src 'self'; style-src 'self'; frame-src 'self'"
+		"default-src 'self'; font-src 'self'; img-src 'self' https://vignette.wikia.nocookie.net/ https://starwars-visualguide.com/; script-src 'self'; style-src 'self'; frame-src 'self'"
 	);
 	next();
 });
