f785238542590482828b6dc6ce32ac3891a85b5e
Chris Burton <chris@fullstackcaveman.com>
2021-06-16 23:16:05 -0600

    wip

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/client/build')));
 
-	app.get('/*', (req, res) =>
+	app.use('/*', (req, res) =>
 		res.sendFile(path.resolve(__dirname, '/client/build/index.html'))
 	);
 } else {
