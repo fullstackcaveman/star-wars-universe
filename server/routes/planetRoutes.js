732f63362a7a097e1cd2342a44c907fcddd66def
Chris Burton <chris@fullstackcaveman.com>
2021-06-20 12:00:15 -0600

    server production

--- server/routes/planetRoutes.js
+++ server/routes/planetRoutes.js
@@ -10,13 +10,12 @@ import {
 } from '../controllers/planetController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').get(getPlanets).post(protect, admin, createPlanet);
+router.route('/info/:pretty_url').get(getPlanetByName);
 router
 	.route('/:id')
 	.get(getPlanetById)
 	.delete(protect, admin, deletePlanet)
 	.put(protect, admin, updatePlanet);
-
-router.route('/info/:pretty_url').get(getPlanetByName);
+router.route('/').get(getPlanets).post(protect, admin, createPlanet);
 
 export default router;
